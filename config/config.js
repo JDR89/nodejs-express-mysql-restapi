36c60eecb045cb3e9f6ae7dc1b6ec72806afa380
JDR89 <joaquin.romero89@gmail.com>
2025-06-26 19:13:43 -0300

    arreglo base de datos

--- config/config.js
+++ config/config.js
@@ -1,12 +1,14 @@
 import { config } from "dotenv";
 
-config();
+config(); // Esto sigue siendo útil para cargar tu .env local
 
-const PORT = process.env.PORT || 3000;
-const DB_HOST= process.env.DB_HOST || "localhost";
-const DB_PORT= process.env.DB_PORT || 3306;
-const DB_USER= process.env.DB_USER || "root";
-const DB_PASSWORD= process.env.DB_PASSWORD || "admin";
-const DB_DATABASE= process.env.DB_DATABASE || "company_db";
+const PORT = process.env.PORT || 4000;
+// --- CAMBIOS AQUÍ ---
+const DB_HOST= process.env.MYSQLHOST || "localhost"; // Ahora busca MYSQLHOST
+const DB_PORT= process.env.MYSQLPORT || 3306; // Ahora busca MYSQLPORT
+const DB_USER= process.env.MYSQLUSER || "root"; // Ahora busca MYSQLUSER
+const DB_PASSWORD= process.env.MYSQLPASSWORD || "admin"; // Ahora busca MYSQLPASSWORD
+const DB_DATABASE= process.env.MYSQLDATABASE || "company_db"; // Ahora busca MYSQLDATABASE
+// -------------------
 
 export {PORT,DB_HOST,DB_PORT,DB_USER,DB_PASSWORD,DB_DATABASE};
